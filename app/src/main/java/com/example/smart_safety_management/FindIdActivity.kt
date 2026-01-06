f9f550a3def3b0e1fe4f1cf75d2b88d214038e4a
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-06 18:40:21 +0900

    아이디 찾기 UI

--- app/src/main/java/com/example/smart_safety_management/FindIdActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/FindIdActivity.kt
@@ -1,11 +1,20 @@
 package com.example.smart_safety_management
 
+import android.graphics.Paint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FindIdActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.find_id)
+
+        // 비밀번호 찾기 텍스트
+        val findPassword = findViewById<TextView>(R.id.tv_find_password)
+
+        // 밑줄 추가
+        findPassword.paintFlags =
+            findPassword.paintFlags or Paint.UNDERLINE_TEXT_FLAG
         }
 }
