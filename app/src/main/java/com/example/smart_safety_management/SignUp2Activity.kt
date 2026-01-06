56cda51a5f56ec79ddd4fc116889c06451b98bcd
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-06 14:39:32 +0900

    회원가입 인증번호 입력, 다음 버튼 상호작용

--- app/src/main/java/com/example/smart_safety_management/SignUp2Activity.kt
+++ app/src/main/java/com/example/smart_safety_management/SignUp2Activity.kt
@@ -1,6 +1,8 @@
 package com.example.smart_safety_management
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUp2Activity : AppCompatActivity() {
@@ -10,6 +12,13 @@ class SignUp2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_2)
+
+        val nextButton = findViewById<Button>(R.id.next_button)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, SignUp3Activity::class.java)
+            startActivity(intent)
+        }
 
         // String -> enum 복원
         userRole = UserRole.valueOf(
@@ -32,5 +41,12 @@ class SignUp2Activity : AppCompatActivity() {
 
     private fun setupManagerUI() {
         // 관리자용 UI / 로직
+    }
+
+    // sign_up_3.xml로 이동 + enum 유지
+    private fun moveToSignUp3() {
+        val intent = Intent(this, SignUp3Activity::class.java)
+        intent.putExtra(EXTRA_USER_ROLE, userRole.name) // enum 전달
+        startActivity(intent)
     }
 }
