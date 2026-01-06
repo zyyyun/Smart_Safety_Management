56d1d0668a415832ba9d51d885d2324336828412
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-06 11:29:18 +0900

    타이틀 수정 및 회원가입
    회원가입 버튼 누르면 회원가입 화면 전환
    근로자로 가입하기 UI

--- app/src/main/java/com/example/smart_safety_management/titleActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/titleActivity.kt
@@ -1,5 +1,8 @@
 package com.example.smart_safety_management
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class TitleActivity : AppCompatActivity() {
@@ -7,5 +10,14 @@ class TitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.title)
+
+        // 1. 회원가입 버튼 가져오기
+        val signUpButton = findViewById<Button>(R.id.button)
+
+        // 2. 클릭 이벤트
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, SignUp1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
