8651803638fca051684145e6f93ad89925ec2653
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-06 13:08:54 +0900

    관리자로 회원가입UI 및 뒤로가기 이동

--- app/src/main/java/com/example/smart_safety_management/SignUp1Activity.kt
+++ app/src/main/java/com/example/smart_safety_management/SignUp1Activity.kt
@@ -1,11 +1,18 @@
 package com.example.smart_safety_management
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUp1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_1)
+
+        val backBtn = findViewById<ImageButton>(R.id.imageButton)
+
+        backBtn.setOnClickListener {    // 뒤로가기 버튼 클릭
+            finish()    // 전 화면으로 이동
+        }
     }
 }
