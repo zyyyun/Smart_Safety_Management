b840302467bc4ef758d15d2e388b97c87c1ee88e
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-06 15:54:00 +0900

    회원가입 아이디, 비밀번호 설정

--- app/src/main/java/com/example/smart_safety_management/SignUp3Activity.kt
+++ app/src/main/java/com/example/smart_safety_management/SignUp3Activity.kt
@@ -1,12 +1,38 @@
 package com.example.smart_safety_management
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUp3Activity : AppCompatActivity() {
 
+    private lateinit var userRole: UserRole
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_3)
+
+        // enum 복원
+        userRole = UserRole.valueOf(
+            intent.getStringExtra(EXTRA_USER_ROLE) ?: UserRole.WORKER.name
+        )
+
+        // 뒤로가기 버튼
+        findViewById<ImageButton>(R.id.backButton).setOnClickListener {
+            finish()
+        }
+
+        // 완료 버튼
+        findViewById<Button>(R.id.finish_button).setOnClickListener {
+            moveToSignUp4()
+        }
+    }
+
+    private fun moveToSignUp4() {
+        val intent = Intent(this, SignUp4Activity::class.java)
+        intent.putExtra(EXTRA_USER_ROLE, userRole.name) // enum 유지
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/smart_safety_management/SignUp4Activity.kt
@@ -0,0 +1,12 @@
+package com.example.smart_safety_management
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUp4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_4)
+    }
+}
