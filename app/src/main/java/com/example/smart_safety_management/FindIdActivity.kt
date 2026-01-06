32e6bc3ad983ad1f4a627ce8123e57a533672b6e
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-06 18:59:22 +0900

    아이디 찾기 버튼 상호작용

--- app/src/main/java/com/example/smart_safety_management/FindIdActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/FindIdActivity.kt
@@ -1,7 +1,10 @@
 package com.example.smart_safety_management
 
+import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,5 +19,24 @@ class FindIdActivity: AppCompatActivity() {
         // 밑줄 추가
         findPassword.paintFlags =
             findPassword.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        // 비밀번호 찾기 이동
+        findPassword.setOnClickListener {
+            val intent = Intent(this, FindPasswordActivity::class.java)
+            startActivity(intent)
         }
+
+        // 뒤로가기 버튼
+        val backBtn = findViewById<ImageButton>(R.id.backButton)
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        // 다음 버튼
+        val nextButton = findViewById<Button>(R.id.next_button)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, FoundIdActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/smart_safety_management/FoundIdActivity.kt
@@ -0,0 +1,4 @@
+package com.example.smart_safety_management
+
+class FoundIdActivity {
+}
