81fa6a95926b2d41fb1ec286fef1daa2c09080eb
SanghwaLee <kimjh6602@naver.com>
2026-01-07 16:00:10 +0900

    폰트 추가 / 파일 수정

---
+++ app/src/main/java/com/example/smart_safety_management/ui/theme/type.kt
@@ -0,0 +1,31 @@
+package com.example.smart_safety_management.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.smart_safety_management.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_black, FontWeight.Black)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    defaultFontFamily = Pretendard,
+    body1 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
