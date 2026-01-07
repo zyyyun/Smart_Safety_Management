e4252b387b16dcea78b97c2f1ae297300b20da68
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-07 11:10:17 +0900

    비밀번호 찾기 UI, 버튼 상호작용

--- app/src/main/java/com/example/smart_safety_management/FindPasswordActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/FindPasswordActivity.kt
@@ -1,4 +1,42 @@
 package com.example.smart_safety_management
 
-class FindPasswordActivity {
+import android.content.Intent
+import android.graphics.Paint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class FindPasswordActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.find_password)
+
+        // 아이디 찾기 텍스트
+        val findPassword = findViewById<TextView>(R.id.tv_find_id)
+
+        // 밑줄 추가
+        findPassword.paintFlags =
+            findPassword.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        // 아이디 찾기 이동
+        findPassword.setOnClickListener {
+            val intent = Intent(this, FindIdActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 뒤로가기 버튼
+        val backBtn = findViewById<ImageButton>(R.id.backButton)
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        // 다음 버튼
+        val nextButton = findViewById<Button>(R.id.next_button)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, ChangePasswordActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/smart_safety_management/FoundIdActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/FoundIdActivity.kt
@@ -1,4 +1,42 @@
 package com.example.smart_safety_management
 
-class FoundIdActivity {
+import android.content.Intent
+import android.graphics.Paint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class FoundIdActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.found_id)
+
+        // 로그인 하러 가기 텍스트
+        val findPassword = findViewById<TextView>(R.id.txt_go_to_login)
+
+        // 밑줄 추가
+        findPassword.paintFlags =
+            findPassword.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        // 로그인 이동
+        findPassword.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 뒤로가기 버튼
+        val backBtn = findViewById<ImageButton>(R.id.backButton)
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        // 비밀번호 찾기 버튼
+        val nextButton = findViewById<Button>(R.id.find_password_button)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, FindPasswordActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
