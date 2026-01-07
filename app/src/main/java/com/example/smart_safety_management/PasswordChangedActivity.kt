b15e19d5b27d2ddc588ceba7c0f17a3c524352c9
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-07 14:00:20 +0900

    아래꺼가 비밀번호 변경 화면, 이게 비밀번호 변경 완료 화면

--- app/src/main/java/com/example/smart_safety_management/PasswordChangedActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/PasswordChangedActivity.kt
@@ -1,11 +1,20 @@
 package com.example.smart_safety_management
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class PasswordChangedActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.password_changed)
+
+        val loginButton = findViewById<Button>(R.id.next_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
