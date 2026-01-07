20f1b9bb40b5fb52555f1cd57e09149328c4f37e
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-07 13:48:25 +0900

    비밀번호 변경 완료 UI, 버튼 상호작용

--- app/src/main/java/com/example/smart_safety_management/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/smart_safety_management/ChangePasswordActivity.kt
@@ -1,11 +1,27 @@
 package com.example.smart_safety_management
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class ChangePasswordActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.change_password)
+
+        // 뒤로가기 버튼
+        val backBtn = findViewById<ImageButton>(R.id.backButton)
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        // 변경완료 버튼
+        val nextButton = findViewById<Button>(R.id.finish_button)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, PasswordChangedActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/smart_safety_management/PasswordChangedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smart_safety_management
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PasswordChangedActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.password_changed)
+    }
+}
