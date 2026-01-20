53e249e058a38c0cf0d175a4167082a766b4055d
SanghwaLee <kimjh6602@naver.com>
2026-01-20 11:30:57 +0900

    카메라 상세 페이지 추가

---
+++ app/src/main/java/com/example/smart_safety_management/CamDetail.kt
@@ -0,0 +1,162 @@
+package com.example.smart_safety_management
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.smart_safety_management.ui.theme.Pretendard
+import com.example.smart_safety_management.ui.theme.Smart_Safety_ManagementTheme
+import com.example.smart_safety_management.ui.theme.TextGray5
+import com.example.smart_safety_management.ui.theme.*
+
+data class Caminfo(
+    val name : String,
+    val code : String,
+    val hostname : String,
+    val hostid : String,
+    val hostpw : String,
+    val state : Boolean,
+    val lastconnect : String,
+)
+
+// 카메라 상세 더미 데이터
+val mockCamInfo = Caminfo(
+    name = "CAM01",
+    code = "TCVR2947G729DH49",
+    hostname = "빌라 에코",
+    hostid = "빌라 에코",
+    hostpw = "********",
+    state = true,
+    lastconnect = "2025-03-18 10:35:59"
+)
+
+
+@Composable
+fun CamDetailScreen(
+    onBackClick: () -> Unit = {}
+) {
+    Smart_Safety_ManagementTheme {
+        val isLight = MaterialTheme.colors.isLight
+        val categoryColor = if(isLight) TextGray60 else TextGray
+        val mainColor = if (isLight) TextGray20 else TextGray5
+        val dividerColor = if (isLight) Lightgray else GrayBackground
+
+
+        Scaffold(
+            backgroundColor = MaterialTheme.colors.onPrimary,
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "카메라 상세",
+                            fontWeight = FontWeight.Bold,
+                            color = if (isLight) Color.Black else TextGray5,
+                            fontFamily = Pretendard,
+                            modifier = Modifier.offset(x = (-24).dp),
+                            fontSize = 24.sp
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = onBackClick) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.backicon),
+                                contentDescription = "Back",
+                                tint = if (isLight) Color.Black else TextGray5
+                            )
+                        }
+                    },
+                    backgroundColor = MaterialTheme.colors.onPrimary,
+                    elevation = 0.dp
+                )
+            }
+        ) { paddingValues ->
+            Column(
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(start = 23.dp, end = 23.dp, top = 23.dp, bottom = 35.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Text(
+                    text = "정보",
+                    fontFamily = Pretendard,
+                    fontSize = 16.sp,
+                    color = categoryColor,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+
+                CamInfoItem("이름", mockCamInfo.name, categoryColor, mainColor)
+                CamInfoItem("장치코드", mockCamInfo.code, categoryColor, mainColor)
+                CamInfoItem("호스트 이름", mockCamInfo.hostname, categoryColor, mainColor)
+                CamInfoItem("호스트 ID", mockCamInfo.hostid, categoryColor, mainColor)
+                CamInfoItem("호스트 PW", mockCamInfo.hostpw, categoryColor, mainColor)
+                CamInfoItem("최근 연결", mockCamInfo.lastconnect, categoryColor, mainColor)
+                CamInfoItem("상태", if (mockCamInfo.state) "정상" else "미수신", categoryColor, mainColor)
+
+                Divider(
+                    color = dividerColor,
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 24.dp,bottom=24.dp)
+                )
+                Text(
+                    text = "촬영",
+                    fontFamily = Pretendard,
+                    fontSize = 16.sp,
+                    color = categoryColor,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CamInfoItem(label: String, value: String, labelColor: Color, valueColor: Color) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            fontFamily = Pretendard,
+            fontSize = 18.sp,
+            color = labelColor
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = value,
+            fontFamily = Pretendard,
+            fontSize = 18.sp,
+            color = valueColor,
+            textAlign = TextAlign.End
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Icon(
+            painter = painterResource(id = R.drawable.edit),
+            contentDescription = "edit",
+            tint = TextMedium
+        )
+
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light Mode")
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark Mode")
+@Composable
+fun CamDetailScreenPreview() {
+    CamDetailScreen()
+}
