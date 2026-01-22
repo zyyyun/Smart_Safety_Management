ff76a3fad60155e9bfd43475766b50146badd571
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-01-22 10:27:21 +0900

    역할 별 디폴트 이름 설정

--- app/src/main/java/com/example/smart_safety_management/UserSession.kt
+++ app/src/main/java/com/example/smart_safety_management/UserSession.kt
@@ -2,7 +2,13 @@ package com.example.smart_safety_management
 
 object UserSession {
     var userRole: UserRole = UserRole.WORKER
-    var userName: String = "안정우"
+        set(value) {
+            field = value
+            // 역할이 바뀔 때 해당 역할의 기본 이름으로 설정
+            userName = if (value == UserRole.MANAGER) "안정우" else "이강인"
+        }
+
+    var userName: String = "이강인"
 
     // 관리자용 초대코드 상태
     var isInviteDoneManager: Boolean = false
