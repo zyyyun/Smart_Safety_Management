d2d3d0c2664035414391694eb24328fca008d098
SanghwaLee <kimjh6602@naver.com>
2026-01-27 14:58:32 +0900

    초대하기에서 연락처를 가져오도록 하는 로직 추가

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const updateProfileRouter = require('./update_profile');
 const changePasswordRouter = require('./change_password');
 const createWorkplaceRouter = require('./create_workplace');
 const deleteWorkplaceRouter = require('./delete_workplace');
+const checkRegisteredContactsRouter = require('./check_registered_contacts');
 
 // '/signup' 경로에 대한 요청을 signupRouter로 전달
 app.use('/', signupRouter);
@@ -20,6 +21,7 @@ app.use('/', updateProfileRouter);
 app.use('/', changePasswordRouter);
 app.use('/', createWorkplaceRouter);
 app.use('/', deleteWorkplaceRouter);
+app.use('/', checkRegisteredContactsRouter);
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`서버가 http://0.0.0.0:${PORT} 에서 실행 중입니다.`);
